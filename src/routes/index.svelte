6126d1218d593bcd6461ea5154ddada9209d0a95
teo.yu <developer.1px@gmail.com>
2022-02-06 12:12:05 +0900

    📦️ adorable-css 설치, style 추가

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+import "@adorable.css"
+import "../style.css"
+</script>
+
+<div class="layer pack">
+  <h1 class="font(36) 700">WORDLE</h1>
+</div>
+
+<svelte:head>
+  <title>테오의 프론트엔드 - Wordle Challenge</title>
+</svelte:head>
