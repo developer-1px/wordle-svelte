70faaf341984417f1ff9674326d42ed92a050bb4
teo.yu <developer.1px@gmail.com>
2022-02-08 20:59:35 +0900

    💄 키보드 flexbox에서 grid로 변경 (줄 간격이 안 맞는거 거슬리네요;;)

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -138,34 +138,27 @@ const onkeydown = (event) => {
   </div>
 
   <!-- Keyboard -->
-  <div class="vbox gap(8) p(8) uppercase">
-    <div class="hbox gap(6)">
-      {#each key1 as key}
-        <KeyButton on:click={() => pushLetter(key)} type={matchedLetters[key]}>{key}</KeyButton>
-      {/each}
-    </div>
+  <div class="grid grid-template-columns(repeat(20,1fr)) p(8) gap(6) uppercase">
+    {#each key1 as key}
+      <KeyButton class="grid-column(span/2)" on:click={() => pushLetter(key)} type={matchedLetters[key]}>{key}</KeyButton>
+    {/each}
 
-    <div class="hbox gap(6)">
-      <div class="flex(.4)"/>
-      {#each key2 as key}
-        <KeyButton on:click={() => pushLetter(key)} type={matchedLetters[key]}>{key}</KeyButton>
-      {/each}
-      <div class="flex(.4)"/>
-    </div>
+    <div class="grid-column(span/1)"/>
+    {#each key2 as key}
+      <KeyButton class="grid-column(span/2)" on:click={() => pushLetter(key)} type={matchedLetters[key]}>{key}</KeyButton>
+    {/each}
+    <div class="grid-column(span/1)"/>
 
-    <div class="hbox gap(6)">
-      <KeyButton class="flex(1.5)" tabindex="-1">ENTER</KeyButton>
-      {#each key3 as key}
-        <KeyButton on:click={() => pushLetter(key)} type={matchedLetters[key]}>{key}</KeyButton>
-      {/each}
-      <KeyButton class="flex(1.5)" tabindex="-1" on:click={backspace}>
-        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
-          <path fill="var(--color-tone-1)" d="M22 3H7c-.69 0-1.23.35-1.59.88L0 12l5.41 8.11c.36.53.9.89 1.59.89h15c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H7.07L2.4 12l4.66-7H22v14zm-11.59-2L14 13.41 17.59 17 19 15.59 15.41 12 19 8.41 17.59 7 14 10.59 10.41 7 9 8.41 12.59 12 9 15.59z"></path>
-        </svg>
-      </KeyButton>
-    </div>
+    <KeyButton class="grid-column(span/3)" tabindex="-1">ENTER</KeyButton>
+    {#each key3 as key}
+      <KeyButton class="grid-column(span/2)" on:click={() => pushLetter(key)} type={matchedLetters[key]}>{key}</KeyButton>
+    {/each}
+    <KeyButton class="grid-column(span/3)" tabindex="-1" on:click={backspace}>
+      <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
+        <path fill="var(--color-tone-1)" d="M22 3H7c-.69 0-1.23.35-1.59.88L0 12l5.41 8.11c.36.53.9.89 1.59.89h15c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H7.07L2.4 12l4.66-7H22v14zm-11.59-2L14 13.41 17.59 17 19 15.59 15.41 12 19 8.41 17.59 7 14 10.59 10.41 7 9 8.41 12.59 12 9 15.59z"></path>
+      </svg>
+    </KeyButton>
   </div>
-
 </div>
 
 <svelte:head>
